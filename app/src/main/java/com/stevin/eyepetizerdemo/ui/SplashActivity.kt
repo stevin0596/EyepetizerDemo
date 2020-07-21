3c48d326a533f259ac2d23247c07237e4cac0246
stevin0596 <17896440596@163.com>
2020-07-21 17:54:32 +0800

    EyePetizer init 2

---
+++ app/src/main/java/com/stevin/eyepetizerdemo/ui/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.stevin.eyepetizerdemo.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.stevin.eyepetizerdemo.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
